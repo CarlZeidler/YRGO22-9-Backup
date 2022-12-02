301b266fbdf8f31a6a9b8ae9140969713df81193
GVBååth <gustav.baath02@gmail.com>
2022-12-02 10:52:00 +0100

    Revert "Merge branch 'main' of https://github.com/CarlZeidler/YRGO22-9-Backup"
    This reverts commit c01fbee6468ad750e5cc9fb016b9323e5c193ede, reversing
    changes made to 84def3a7b53a647b5179b81319b29d7baf65fceb.

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -167,7 +167,6 @@ public class PlayerMove : MonoBehaviour
         if (Grounded() & !Input.GetButton("Jump"))
         {
             _jumpDurationLeft = jumpDurationLeft;
-            animspeedLocked = false;
         }
         if (canMove)
         {
@@ -177,7 +176,6 @@ public class PlayerMove : MonoBehaviour
                 aud.Stop();
                 aud.clip = jump;
                 aud.Play();
-                animspeedLocked = true;
             }
 
             if (Input.GetButton("Jump") && _jumpDurationLeft > 0)
