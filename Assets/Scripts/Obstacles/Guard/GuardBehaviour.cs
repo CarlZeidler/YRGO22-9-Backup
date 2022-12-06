b0eb244cd93025ac8347b9b9b52c4d8060dc8a4c
CarlZeidler <carl.zeidler1@gmail.com>
2022-12-06 12:27:52 +0100

    Updated Guards detection area

--- Assets/Scripts/Obstacles/Guard/GuardBehaviour.cs
+++ Assets/Scripts/Obstacles/Guard/GuardBehaviour.cs
@@ -6,7 +6,34 @@ public class GuardBehaviour : HackableObjects
 {
     public GuardMove moveScript;
     public GuardVision visionScript;
-    
+
+    [SerializeField] private float killTime = 1f;
+    [SerializeField] private bool pInRange = false;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer detectionAreaSprite;
+    [SerializeField] private Color detectionColor, activeColor, inactiveColor;
+    [SerializeField] LayerMask ignoreLayer;
+
+    private void Update()
+    {
+        if (pInRange)
+        {
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, (GameManager.instance.player.transform.position - transform.position), Mathf.Infinity, ~ignoreLayer);
+            if (ray.collider.gameObject.layer == GameManager.instance.player.layer && pInRange)
+            {
+                detectionAreaSprite.color = detectionColor;
+                Invoke(nameof(Death), killTime);
+                Invoke(nameof(Shoot), killTime - killTime / 8);
+
+            }
+        }
+        else
+        {
+            CancelInvoke(nameof(Death));
+            CancelInvoke(nameof(Shoot));
+        }
+    }
+
     public void Shutdown()
     {
         moveScript.canMove = false;
@@ -19,5 +46,54 @@ public class GuardBehaviour : HackableObjects
         //Restore functionality when the hacking time is over.
         visionScript.canSee = true;
         moveScript.canMove = true;
+    }
+
+    public void OnPlayerEnter()
+    {
+        pInRange = true;
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, (GameManager.instance.player.transform.position - transform.position), Mathf.Infinity, ~ignoreLayer);
+        if (ray.collider.gameObject.layer == GameManager.instance.player.layer && pInRange)
+        {
+            detectionAreaSprite.color = detectionColor;
+            Invoke(nameof(Death), killTime);
+            Invoke(nameof(Shoot), killTime - killTime / 8);
+
+        }
+    }
+
+    public void OnPlayerExit()
+    {
+        pInRange = false;
+        detectionAreaSprite.color = activeColor;
+        CancelInvoke(nameof(Death));
+        CancelInvoke(nameof(Shoot));
+    }
+
+    public void TriggerExit()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, (GameManager.instance.player.transform.position - transform.position), Mathf.Infinity, ~ignoreLayer);
+        if (ray.collider.gameObject.layer == GameManager.instance.player.layer && pInRange)
+        {
+            detectionAreaSprite.color = detectionColor;
+            Invoke(nameof(Death), killTime);
+            Invoke(nameof(Shoot), killTime - killTime / 8);
+
+        }
+    }
+
+    private void Death()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, (GameManager.instance.player.transform.position - transform.position), Mathf.Infinity, ~ignoreLayer);
+        if (ray.collider.gameObject.layer == GameManager.instance.player.layer && pInRange)
+        {
+            GameManager.instance.player.GetComponent<PlayerRespawn>().Respawn();
+
+        }
+    }
+
+    private void Shoot()
+    {
+        animator.speed = 2;
+        animator.SetTrigger("Shoot");
     }
 }
