3766f6257d8884f0abd2e62b16891f3d8d1544d1
GVBååth <gustav.baath02@gmail.com>
2022-12-15 10:41:13 +0100

    added support for audiomixing across scenes

--- Assets/Scripts/ErikScripts/SettingsMenu.cs
+++ Assets/Scripts/ErikScripts/SettingsMenu.cs
@@ -2,22 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+
+    private void Awake()
+    {
+        //set value on slider
+        try
+        {
+            GameObject.Find("MasterVolumeSlider").GetComponentInChildren<Slider>().value = PlayerPrefs.GetFloat("masterVolume");
+            GameObject.Find("MusicVolumeSlider").GetComponentInChildren<Slider>().value = PlayerPrefs.GetFloat("musicVolume");
+            GameObject.Find("SfxVolumeSlider").GetComponentInChildren<Slider>().value = PlayerPrefs.GetFloat("sfxVolume");
+        }
+        catch { }
+        audioMixer.SetFloat("masterVolume", (PlayerPrefs.GetFloat("masterVolume")));
+        audioMixer.SetFloat("musicVolume", (PlayerPrefs.GetFloat("musicVolume")));
+        audioMixer.SetFloat("sfxVolume", (PlayerPrefs.GetFloat("sfxVolume")));
+    }
     public void SetMasterVolume(float sliderVal)
     {
         audioMixer.SetFloat("masterVolume", sliderVal);
+        PlayerPrefs.SetFloat("masterVolume", (sliderVal));
     }
     public void SetMusicVolume(float sliderVal)
     {
         audioMixer.SetFloat("musicVolume", sliderVal);
+        PlayerPrefs.SetFloat("musicVolume", (sliderVal));
     }
     public void SetSfxVolume(float sliderVal)
     {
         audioMixer.SetFloat("sfxVolume", sliderVal);
+        PlayerPrefs.SetFloat("sfxVolume", (sliderVal));
     }
 
     public void SetQuality(int qualityIndex)
